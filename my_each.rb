505268d83899f3431824041acc6b1f496dc15fb9
FutureScript <fernandobaxter1@gmail.com>
2018-09-28 01:48:54 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,4 +1,12 @@
-def my_each # put argument(s) here
+#def my_each # put argument(s) here
   # code here
-end
+#end
 
+def my_each(array)
+  index = 0
+  while index<array.length
+    array[index] = yield(array[index])
+    index+=1
+  end
+  array
+end
